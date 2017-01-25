946cfcfab12abe53c3c950cf4d3d88f7f586e8d4
Davi Custodio <davi.custodio@embrapa.br>
2017-01-25 07:42:46 -0200

    search box in tab all layers

--- src/makeTabs.js
+++ src/makeTabs.js
@@ -14,13 +14,13 @@ var MakeTabs = L.Class.extend({
 		}]
 	},
 
-    initialize: function (placeholder, options) {
+    initialize: function (placeholder, options, place_id) {
+    	this.place_id = place_id ? place_id : "layerManagement_tabs";
     	this.options = options ? options : this.options;
-    	this._placeholder = placeholder;
+    	this.placeholder = placeholder;
     	// make the linkTemplates
     	this._linkTemplates = document.querySelector("[rel='import']");
     	this.createTabs();
-    	this.createAllLayers();
     },
 
     createTabs: function(){
@@ -28,42 +28,45 @@ var MakeTabs = L.Class.extend({
     	// create id filed in options tabs and concat the placeholder to the type
     	var _this = this;
         this.options.tabs.forEach(function(item){
-        	item.id = item.type + "_" + _this._placeholder;
+        	item.id = item.type + "_" + _this.placeholder;
+        	item.ref_id = "ref" + item.id;
         });
 
         // subst id from layerManagement_tabs
-        var id_tabs = this._placeholder + '-layerManagement_tabs';
-        $("#layerManagement_tabs").attr('id',id_tabs);
+        var id_tabs = this.placeholder + '-' + this.place_id;
+        $("#" + this.place_id).attr('id',id_tabs);
+        id_tabs = "#" + id_tabs;
         
         // get the templates for tabs and tabs_detail
-        var tabs = $.parseHTML(this._linkTemplates.import.querySelector('#template_tabs').innerHTML);
-        var tabs_detail = this._linkTemplates.import.querySelector('#template_tabs_detail');
+        var tabs = this._linkTemplates.import.querySelector('#template_tabs').innerHTML;
+        // send template to DOM
+        $(id_tabs).html(tabs);
 
-        // apply the template
-        tabs_detail = $.tmpl( tabs_detail.innerHTML, this.options.tabs );
-        // insert tab itens in tabs tag
-        $(tabs).find(".nav").append(tabs_detail);
-        $("#"+id_tabs).html(tabs);
+        // apply template in li reference
+        var tabs_compiled = $.tmpl( $(id_tabs).find('ul'), this.options.tabs );
+        $(id_tabs).find('ul').html(tabs_compiled);
 
-        // create refs to the elements (by id) of tabs to insert child 
-        this.options.tabs.forEach(function(item){
-        	item.element = $("#"+item.id);
-        });
+        // apply template in tab-content reference
+        var tabs_compiled = $.tmpl( $(id_tabs).find('.tab-content'), this.options.tabs );
+        $(id_tabs).find('.tab-content').html(tabs_compiled);
+
+        this.createSearchInAllLayers();
 
     },
 
-    createAllLayers: function(){
+    createSearchInAllLayers: function(){
 
-        // get the dom element that ref to the tab  
-    	var element = this.options.tabs.filter(function(item){
+        // get the id from element that ref to the tab All Layers
+        var id_tab;
+    	this.options.tabs.forEach(function(item){
     		if( item.type == "all_layers"){
-    			return item.element;
+    			id_tab = "#"+item.ref_id;
     		}
     	});
 
     	// get template for the search field
     	var search = $.parseHTML(this._linkTemplates.import.querySelector('#template_tab_all_layers_search').innerHTML);
-    	$(element.element).html(search);
+    	$(id_tab).append(search);
 
     }
     
